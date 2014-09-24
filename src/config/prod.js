d1b4cd04b8d4274c318f712ace6d6e25fbe69ed6
Alex Deaconu <alex.deaconu@risevision.com>
2014-09-24 11:30:47 -0400

    [feature] Add Store Authorization component
    Updated references to include the component from widget-common
    - Component call gets initialized in parallel to data api call
    - This way in case of auth failure, api sends dummy data
    - If Widget is not authorized, data retrieval is blocked, but all data timers still run (in case it gets authorized in the future)
    
    Updated config so it can be used by pure js widget
    Retrieving company Id from viewer for preview

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,6 +1,12 @@
 "use strict";
 
-angular.module("risevision.widget.financialTable.config", [])
-  .value("PRODUCT_CODE", "1c2acf0b1e789bf4f14506e3e27c8a32832b6d5a")
-  .value("PRODUCT_ID", "13")
-;
+var FINANCIAL_TABLE_CONFIG = {
+  PRODUCT_CODE: "1c2acf0b1e789bf4f14506e3e27c8a32832b6d5a"
+};
+
+if (typeof angular !== "undefined") {
+  angular.module("risevision.widget.financialTable.config", [])
+    .value("PRODUCT_CODE", FINANCIAL_TABLE_CONFIG.PRODUCT_CODE)
+    .value("PRODUCT_ID", "13")
+  ;
+}
